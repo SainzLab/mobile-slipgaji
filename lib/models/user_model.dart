081371706c9c9e596ac87fdbf7c5dc8583d4e39c
FazliXZ9 <fazlia322sok@gmail.com>
2026-02-20 23:51:18 +0700

    updt models

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -8,11 +8,10 @@ class User {
   User({required this.id, required this.nip, required this.nama, required this.email, this.token = ""});
 
   factory User.fromJson(Map<String, dynamic> json, {String? token}) {
-    // Mapping sesuai struktur JSON Apidog 'user' object
     return User(
       id: json['id'],
       nip: json['nip'],
-      nama: json['nama_pegawai'], // Sesuai JSON: nama_pegawai
+      nama: json['nama_pegawai'],
       email: json['email'],
       token: token ?? "",
     );
